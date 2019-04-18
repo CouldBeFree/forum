5e70698e163c0107851a3940338fbe8a3b966751
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-04-18 17:59:53 +0300

    Validation

--- components/Signup.vue
+++ components/Signup.vue
@@ -11,30 +11,39 @@
                 <v-text-field
                         v-model="name"
                         :counter="20"
-                        :rules="nameRules"
+                        :error-messages="nameErrors"
                         label="Name"
                         required
+                        @input="$v.name.$touch()"
+                        @blur="$v.name.$touch()"
                 ></v-text-field>
 
                 <v-text-field
                         v-model="email"
-                        :rules="emailRules"
+                        :error-messages="emailErrors"
                         label="E-mail"
                         required
+                        @input="$v.email.$touch()"
+                        @blur="$v.email.$touch()"
                 ></v-text-field>
 
                 <v-text-field
                         v-model="password"
-                        :type="'password'"
+                        :counter="20"
+                        :error-messages="passwordErrors"
                         label="Password"
                         required
+                        @input="$v.password.$touch()"
+                        @blur="$v.password.$touch()"
                 ></v-text-field>
 
                 <v-checkbox
                         v-model="checkbox"
-                        :rules="[v => !!v || 'You must agree to continue!']"
+                        :error-messages="checkboxErrors"
                         label="Do you agree?"
                         required
+                        @change="$v.checkbox.$touch()"
+                        @blur="$v.checkbox.$touch()"
                 ></v-checkbox>
                 <v-btn
                         color="error"
@@ -51,7 +60,7 @@
                 </v-btn>
                 <v-btn
                         @click="submit"
-                        :disabled="valid"
+                        :disabled="isValid"
                 >
                     Submit
                 </v-btn>
@@ -63,11 +72,14 @@
 
 <script>
     import { mapMutations } from 'vuex';
+    import { validationMixin } from 'vuelidate'
+    import { required, maxLength, email } from 'vuelidate/lib/validators'
 
     export default {
+        mixins: [validationMixin],
+
         data: () => ({
-            valid: true,
-            password: '',
+            /*password: '',
             name: '',
             nameRules: [
                 v => !!v || 'Name is required',
@@ -78,8 +90,26 @@
                 v => !!v || 'E-mail is required',
                 v => /.+@.+/.test(v) || 'E-mail must be valid'
             ],
-            checkbox: false
+            checkbox: false*/
+            valid: false,
+            name: '',
+            email: '',
+            select: null,
+            checkbox: false,
+            password: ''
         }),
+
+        validations: {
+            name: { required, maxLength: maxLength(20) },
+            email: { required, email },
+            select: { required },
+            password: { required },
+            checkbox: {
+                checked (val) {
+                    return val
+                }
+            }
+        },
 
         methods: {
             reset () {
@@ -87,14 +117,59 @@
             },
             ...mapMutations([
                 'toggleSignUp'
-            ])
+            ]),
+            submit () {
+                this.$v.$touch();
+                if(!this.emailErrors.length || !this.nameErrors.length || !this.passwordErrors.length || !this.checkboxErrors.length){
+                    alert('Valid')
+                }
+            },
+            clear () {
+                this.$v.$reset(),
+                    this.name = '',
+                    this.email = '',
+                    this.password = '',
+                    this.checkbox = false
+            }
         },
         computed: {
-            validate () {
+            /*validate () {
                 if (this.emailRules && this.checkbox && this.name) {
                     this.valid = true
                 }
+            }*/
+            checkboxErrors () {
+                const errors = [];
+                if (!this.$v.checkbox.$dirty) return errors;
+                !this.$v.checkbox.checked && errors.push('You must agree to continue!');
+                return errors
             },
+            passwordErrors () {
+                const errors = [];
+                if (!this.$v.name.$dirty) return errors;
+                !this.$v.name.maxLength && errors.push('Password must be at most 10 characters long');
+                !this.$v.name.required && errors.push('Password is required.');
+                return errors
+            },
+            nameErrors () {
+                const errors = [];
+                if (!this.$v.name.$dirty) return errors;
+                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long');
+                !this.$v.name.required && errors.push('Name is required.');
+                return errors
+            },
+            emailErrors () {
+                const errors = [];
+                if (!this.$v.email.$dirty) return errors;
+                !this.$v.email.email && errors.push('Must be valid e-mail');
+                !this.$v.email.required && errors.push('E-mail is required');
+                return errors
+            },
+            isValid(){
+                if(!this.emailErrors.length && !this.nameErrors.length && !this.passwordErrors.length && !this.checkboxErrors.length){
+                    this.valid = true
+                }
+            }
         }
     }
 </script>
