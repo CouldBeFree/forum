a2882926678957bdf7fa7def25438fd27ce8c249
CouldBeFree <propokerstar90@gmail.com>
2019-04-21 21:26:30 +0300

    asyncData

---
+++ components/TopicList.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-list>
+        <v-list-tile>
+            <v-list-tile-content>
+                <nuxt-link :to="'/category/'">Test</nuxt-link>
+            </v-list-tile-content>
+            <h1>Test {{category}}</h1>
+            <h1>I am topic list</h1>
+            <button @click="test">Test click</button>
+        </v-list-tile>
+    </v-list>
+</template>
+
+<script>
+    import axios from '~/plugins/axios';
+
+    export default {
+        data () {
+            return {
+                category: 'Some category'
+            }
+        },
+        asyncData() {
+            return axios.get('category')
+                .then((res) => {
+                    return {category: res.data}
+                })
+        },
+        methods:{
+            test(){
+                axios.get('/category').then(res => {
+                    this.category = res.data
+                })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,33 @@
 <template>
-  <div>
-    <div class="form-holder" v-if="formSignup">
-      <Signup/>
+    <div>
+        <div class="form-holder" v-if="formSignup">
+            <Signup/>
+        </div>
+        <div class="form-holder" v-if="formLogin">
+            <Login/>
+        </div>
+        <TopicList/>
     </div>
-    <div class="form-holder" v-if="formLogin">
-      <Login/>
-    </div>
-  </div>
 </template>
 
 <script>
-  import Signup from '~/components/Signup.vue';
-  import Login from '~/components/Login.vue';
+    import Signup from '~/components/Signup.vue';
+    import Login from '~/components/Login.vue';
+    import TopicList from '~/components/TopicList.vue';
 
-  export default {
-    components: {
-      Signup,
-      Login
-    },
-    computed: {
-      formSignup() {
-        return this.$store.state.signUp
-      },
-      formLogin() {
-        return this.$store.state.login
-      }
+    export default {
+        components: {
+            Signup,
+            Login,
+            TopicList
+        },
+        computed: {
+            formSignup() {
+                return this.$store.state.signUp
+            },
+            formLogin() {
+                return this.$store.state.login
+            }
+        }
     }
-  }
 </script>
